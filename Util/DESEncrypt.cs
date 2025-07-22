f4e78cc414a7094200f44ce95531c675e2474d01
再见理想 <crazpro@hotmail.com>
2025-07-22 14:57:44 +0800

    1.加密解密类

---
+++ Util/DESEncrypt.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DoPENetConnect
+{
+    /// <summary>
+    /// DES加密/解密类。
+    /// </summary>
+    public class DESEncrypt
+    {
+
+        private static string strKey = "XINCE";
+
+        public DESEncrypt()
+        {
+        }
+
+        #region ========加密======== 
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static string Encrypt(string Text)
+        {
+            return Encrypt(Text, strKey);
+        }
+
+
+        /// <summary> 
+        /// 加密数据 
+        /// </summary> 
+        /// <param name="Text"></param> 
+        /// <param name="sKey"></param> 
+        /// <returns></returns> 
+        public static string Encrypt(string Text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray;
+            inputByteArray = Encoding.Default.GetBytes(Text);
+
+            string strByteKey = GetMd5Hash(sKey).ToUpper().Substring(0, 8);
+
+            des.Key = ASCIIEncoding.ASCII.GetBytes(strByteKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(strByteKey);
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            return ret.ToString();
+        }
+
+        #endregion
+
+        #region ========解密======== 
+
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static string Decrypt(string Text)
+        {
+            return Decrypt(Text, strKey);
+        }
+
+
+        /// <summary> 
+        /// 解密数据 
+        /// </summary> 
+        /// <param name="Text"></param> 
+        /// <param name="sKey"></param> 
+        /// <returns></returns> 
+        public static string Decrypt(string Text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            int len;
+            len = Text.Length / 2;
+            byte[] inputByteArray = new byte[len];
+            int x, i;
+            for (x = 0; x < len; x++)
+            {
+                i = Convert.ToInt32(Text.Substring(x * 2, 2), 16);
+                inputByteArray[x] = (byte)i;
+            }
+
+            string strByteKey = GetMd5Hash(sKey).ToUpper().Substring(0, 8);
+
+            des.Key = ASCIIEncoding.ASCII.GetBytes(strByteKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(strByteKey);
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            return Encoding.Default.GetString(ms.ToArray());
+        }
+
+        #endregion
+
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                // 将输入字符串转换为字节数组并计算哈希值
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // 将字节数组转换为十六进制字符串
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2")); // 转换为小写的十六进制格式
+                }
+                return sb.ToString();
+            }
+        }
+
+
+    }
+
+
+}
+
