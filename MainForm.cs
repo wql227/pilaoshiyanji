39d2b510063add4922f7d929f79e517d14a23d4e
再见理想 <crazpro@hotmail.com>
2025-08-06 14:27:36 +0800

    1.导入测试数据。

--- MainForm.cs
+++ MainForm.cs
@@ -3302,8 +3302,61 @@ namespace DoPENetConnect
         /// <param name="e"></param>
         private void ToolStripMenuItem_OpenLogsDir_Click(object sender, EventArgs e)
         {
-            string strLogFilePath = "Logs";
-            System.Diagnostics.Process.Start(System.IO.Directory.GetCurrentDirectory() + "\\"+ strLogFilePath);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
+            openFileDialog.Filter = "CSV文件 (*.csv)|*.csv"; // 如果需要筛选特定类型的文件，如CSV
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                // 使用选中的文件路径进行操作
+
+                if (!string.IsNullOrEmpty(selectedFilePath))
+                {
+                    ExcelHelper excelHelper = new ExcelHelper(selectedFilePath);
+
+                    DataTable trCsvData = excelHelper.CSVToDataTable(false);
+
+                    DataPoint dpPos = null;
+                    DataPoint dpLoad = null;
+                    DataPoint dpExt = null;
+                    DataPoint dpCommand = null;
+
+                    //List<DataPoint> points = new List<DataPoint>();
+                    chart_machine.Series[0].Points.Clear();
+                    chart_machine.Series[1].Points.Clear();
+                    chart_machine.Series[2].Points.Clear();
+                    chart_machine.Series[3].Points.Clear();
+
+                    if (trCsvData != null && trCsvData.Rows.Count >= 1)
+                    {
+                        for (int i = 0; i < trCsvData.Rows.Count; i++)
+                        {
+                            double strX = double.Parse(trCsvData.Rows[i][0].ToString());
+                            double strYPos = double.Parse(trCsvData.Rows[i][1].ToString());
+                            double strYLoad = double.Parse(trCsvData.Rows[i][2].ToString());
+                            double strYExt = double.Parse(trCsvData.Rows[i][3].ToString());
+                            double strYCommand = double.Parse(trCsvData.Rows[i][4].ToString());
+
+                            dpPos = new DataPoint(strX, strYPos);
+                            dpLoad = new DataPoint(strX, strYLoad);
+                            dpExt = new DataPoint(strX, strYExt);
+                            dpCommand = new DataPoint(strX, strYCommand);
+      
+                            chart_machine.Series[0].Points.Add(dpPos);
+                            chart_machine.Series[1].Points.Add(dpLoad);
+                            chart_machine.Series[2].Points.Add(dpExt);
+                            chart_machine.Series[3].Points.Add(dpCommand);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("所选的文件内无数据！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                }
+            }
+
         }
 
 
@@ -3773,6 +3826,11 @@ namespace DoPENetConnect
             {
                 MessageBox.Show("请先暂停绘制！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
+        }
+
+        private void ToolStripMenuItem_OpenLangDir_Click(object sender, EventArgs e)
+        {
 
         }
     }
