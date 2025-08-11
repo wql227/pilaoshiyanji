127ad4df1e9780605fabf9711d3cf99f358dbd45
glm-001 <pingguo1014@163.com>
2025-08-11 17:01:54 +0800

    取消开始时曲线清除操作

--- MainForm.cs
+++ MainForm.cs
@@ -2761,7 +2761,7 @@ namespace DoPENetConnect
 
                 //开始前把曲线x轴调到0点
                 //x_Position = 0;
-                InitCurveBeforeExperment();           //开始实验前初始化绘图，包括x轴调整至0点
+                //InitCurveBeforeExperment();           //开始实验前初始化绘图，包括x轴调整至0点
                 isRunning = true;
                 SetControlEnable(false);
                 //nTestCount = HalfCycles;
