3c108f697b7d137c989fe82e14b74ef14691a98f
glm-001 <pingguo1014@163.com>
2025-08-25 17:01:43 +0800

    数据加载---目前有问题

--- MainForm.cs
+++ MainForm.cs
@@ -434,7 +434,7 @@ namespace DoPENetConnect
             EnableButton();
 
             // Connect to EDC
-            ConnectToEdc();
+            //ConnectToEdc();
 
             //设置lightningchart参数
             CreateChart();
@@ -3916,8 +3916,107 @@ namespace DoPENetConnect
         /// <param name="e"></param>
         private void ToolStripMenuItem_OpenLogsDir_Click(object sender, EventArgs e)
         {
-            string strLogFilePath = "Logs";
-            System.Diagnostics.Process.Start(System.IO.Directory.GetCurrentDirectory() + "\\" + strLogFilePath);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
+            openFileDialog.Filter = "CSV文件 (*.csv)|*.csv"; // 如果需要筛选特定类型的文件，如CSV
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                // 使用选中的文件路径进行操作
+
+                if (!string.IsNullOrEmpty(selectedFilePath))
+                {
+                    ExcelHelper excelHelper = new ExcelHelper(selectedFilePath);
+
+                    DataTable trCsvData = excelHelper.CSVToDataTableStatic(true);
+
+                    //foreach (DataRow dr2 in trCsvData.Rows)
+                    //{
+                    //    Console.WriteLine(dr2["Time [s]"].ToString() + "<br>");
+                    //}
+                    DataPoint dpPos = null;
+                    DataPoint dpLoad = null;
+                    DataPoint dpExt = null;
+                    DataPoint dpCommand = null;
+                    DataPoint dpLoadPos = null;
+                    DataPoint dpLoadExt = null;
+
+                    //List<DataPoint> points = new List<DataPoint>();
+
+                    for (int i = 0; i < chart_machine.Series[0].Points.Count; i++)
+                    {
+                        chart_machine.Series[0].Points.RemoveAt(i);
+                    }
+                    for (int i = 0; i < chart_machine.Series[1].Points.Count; i++)
+                    {
+                        chart_machine.Series[1].Points.RemoveAt(i);
+                    }
+                    for (int i = 0; i < chart_machine.Series[2].Points.Count; i++)
+                    {
+                        chart_machine.Series[2].Points.RemoveAt(i);
+                    }
+                    for (int i = 0; i < chart_machine.Series[3].Points.Count; i++)
+                    {
+                        chart_machine.Series[3].Points.RemoveAt(i);
+                    }
+
+                    //chart_machine.Series[0].Points.Clear();
+                    //chart_machine.Series[1].Points.Clear();
+                    //chart_machine.Series[2].Points.Clear();
+                    //chart_machine.Series[3].Points.Clear();
+                    //chart_machine.Series[4].Points.Clear();
+                    //chart_machine.Series[5].Points.Clear();
+
+                    if (trCsvData != null && trCsvData.Rows.Count >= 1)
+                    {
+                        for (int i = 0; i < trCsvData.Rows.Count; i++)
+                        {
+                            double strX = double.Parse(trCsvData.Rows[i][0].ToString());
+                            double strYPos = double.Parse(trCsvData.Rows[i][1].ToString());
+                            double strYLoad = double.Parse(trCsvData.Rows[i][2].ToString());
+                            double strYExt = double.Parse(trCsvData.Rows[i][3].ToString());
+                            double strYCommand = double.Parse(trCsvData.Rows[i][4].ToString());
+
+                            dpPos = new DataPoint(strX, strYPos);
+                            dpLoad = new DataPoint(strX, strYLoad);
+                            dpExt = new DataPoint(strX, strYExt);
+                            dpCommand = new DataPoint(strX, strYCommand);
+                            dpLoadPos = new DataPoint(strYLoad, strYPos);
+                            dpLoadExt = new DataPoint(strYLoad, strYExt);
+
+                            chart_machine.Series[0].Points.Add(dpPos);
+                            chart_machine.Series[1].Points.Add(dpLoad);
+                            chart_machine.Series[2].Points.Add(dpExt);
+                            chart_machine.Series[3].Points.Add(dpCommand);
+                            chart_machine.Series[4].Points.Add(dpLoadPos);
+                            chart_machine.Series[5].Points.Add(dpLoadExt);
+
+                            if (maxSeries0 < strYPos) maxSeries0 = strYPos;
+                            if (minSeries0 > strYPos) minSeries0 = strYPos;
+
+                            if (maxSeries1 < strYLoad) maxSeries1 = strYLoad;
+                            if (minSeries1 > strYLoad) minSeries1 = strYLoad;
+
+                            if (maxSeries1 < strYExt) maxSeries2 = strYExt;
+                            if (minSeries1 > strYExt) minSeries2 = strYExt;
+
+                            if (maxSeries3 < strYCommand) maxSeries3 = strYCommand;
+                            if (minSeries3 > strYCommand) minSeries3 = strYCommand;
+
+                            x_Position = strX;
+
+                        }
+
+                        AutoFittingCurve(maxSeries0, minSeries0, maxSeries1, minSeries1, maxSeries2, minSeries2, maxSeries3, minSeries3);
+                        //chart_machine.Invalidate();
+                    }
+                    else
+                    {
+                        MessageBox.Show("所选的文件内无数据！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                }
+            }
         }
 
 
