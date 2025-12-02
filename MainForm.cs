6f75a9878e4d1cca5bb82be6ce473036f2b7b0de
再见理想 <crazpro@hotmail.com>
2025-12-02 11:28:41 +0800

    1.退出问题修改

--- MainForm.cs
+++ MainForm.cs
@@ -4821,21 +4821,7 @@ namespace DoPENetConnect
         /// <param name="e"></param>
         private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("确定要退出程序吗？", "退出确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
-
-            if (result == DialogResult.Yes)
-            {
-                if (bConnected)
-                {
-                    OffEDC();
-                }
-
-                Application.Exit();
-            }
-            else if (result == DialogResult.Cancel)
-            {
-                return;
-            }
+            this.Close();
         }
 
 
@@ -4857,6 +4843,26 @@ namespace DoPENetConnect
                 e.Cancel = true; // 取消关闭
             }
         }
+
+
+        //public void QuitApp()
+        //{
+        //    DialogResult result = MessageBox.Show("确定要退出程序吗？", "退出确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+        //    if (result == DialogResult.Yes)
+        //    {
+        //        bQuit = true;
+
+        //        if (bConnected)
+        //        {
+        //            OffEDC();
+        //        }
+        //    }
+        //    else if (result == DialogResult.No)
+        //    {
+        //        e.Cancel = true; // 取消关闭
+        //    }
+        //}
 
 
         /// <summary>
