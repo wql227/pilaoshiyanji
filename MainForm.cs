dee797b99f50ed90acf12f118e84277059a8e340
再见理想 <crazpro@hotmail.com>
2026-03-06 13:33:00 +0800

    修改位移刷新不显示负数问题

--- MainForm.cs
+++ MainForm.cs
@@ -3437,7 +3437,7 @@ namespace DoPENetConnect
         /// </summary>
         public void SetMemberParam()
         {
-            nCountREfresh = DataRefreshFrequency / (double)SampleFrequency;
+            nCountREfresh = DataRefreshFrequency / (double)SampleFrequency - 1;
 
             if (LoadUnit.ToUpper() == "KN")
             {
